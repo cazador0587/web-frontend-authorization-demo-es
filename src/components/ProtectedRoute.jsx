608a5a9e099545f3403ba6baeb526a291d769c08
Fernando Salvador <fsalvador@ecosistemasmexico.com>
2026-04-22 14:06:03 -0600

    El hook useLocation

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,11 +1,35 @@
-import { Navigate } from "react-router-dom";
+// Nueva importación - useLocation
+import { Navigate, useLocation } from "react-router-dom";
 
-function protectedRoute({ isLoggedIn, children }) {
-  if (!isLoggedIn) {
-    return <Navigate to="/login" replace />;
-  } else {
+// Nueva prop - anonymous. Esta propiedad se utilizará para indicar las rutas
+// que pueden visitarse de forma anónima (es decir, sin autorización).
+// Las dos rutas 'anónimas' de esta aplicación son /register
+// y /login.
+function protectedRoute({
+  isLoggedIn,
+  children,
+  anonymous = false }) {
+    // Invoca el hook useLocation y accede al valor de la propiedad
+    // 'from' de su objeto state. Si no existe la propiedad 'from'
+    // se utilizará por defecto "/".
+    const location = useLocation();
+    const from = location.state?.from || "/";
+    // Si el usuario ha iniciado la sesión le redirigimos fuera de nuestras
+    // rutas anónimas.
+    if (anonymous && isLoggedIn) {
+      return <Navigate to={from} />;
+    }
+    // Si el usuario no ha iniciado la sesión e intenta acceder a una ruta que
+    // requiere autorización, le redirigimos a la ruta /login.
+    if (!anonymous && !isLoggedIn) {
+      // Mientras redirigimos a /login establecemos los objetos location
+      // la propiedad state.from para almacenar el valor de la ubicación actual.
+      // Esto nos permite redirigirles correctamente después de que
+      // inicien sesión.
+      return <Navigate to="/login" state={{ from: location }} />;
+    }
+    // En caso contrario, muestra los hijos de la ruta actual.
     return children;
   }
-}
 
 export default protectedRoute;
